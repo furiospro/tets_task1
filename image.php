2023ce134e8ae77a22e5a65fb9c27bb598e1409a
furiospro86 <furiospro86@gmail.com>
2021-08-28 16:32:05 +1000

    well done

--- image.php
+++ image.php
@@ -1,19 +1,15 @@
 <?php
 header('Content-type:image/jpeg');
+header('Content-type:image/jpeg');
 if(readfile('https://picsum.photos/447/300')){
-	if(file_exists('test.txt')){
-		$f_con = file_get_contents('test.txt');
-		$file = fopen('test.txt','w+');
-		if(is_numeric($f_con)){
-			$f_con++;
-			fwrite($file,$f_con);
-			fclose($file);
-		}else{
-			fwrite($file,1);
-			fclose($file);
-		}
+	$f_con = file_get_contents('test.txt');//Попытка прочитать файл. Если нет - получаем false если файла нет
+	$file = fopen('test.txt','w+');//Либо создается файл и открывается для записи и чтения
+													// либо сразу открывается для записи и чтения
+	if($f_con && is_numeric($f_con)){// сначала проверка, что файл прочитался, потом, что там верные данные(число)
+		$f_con++;
+		fwrite($file,$f_con);
+		fclose($file);
 	}else{
-		$file = fopen('test.txt','w+');
 		fwrite($file,1);
 		fclose($file);
 	}
